c99ad1a8bba75c885c93c561f45a5feb738767df
MainPC <7max009@naver.com>
2023-09-30 03:28:40 +0900

    Drill05 제출 (2020182005 구민상)
    drill 05 제출합니다.

--- line.py
+++ line.py
@@ -1,74 +1,68 @@
-import turtle
 import random
+from pico2d import *
 
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+mouse_pointer = load_image('hand_arrow.png')
 
-def stop():
-    turtle.bye()
+points = [ ( random.randint(0,TUK_WIDTH-75) , random.randint(0,TUK_HEIGHT-750) ) for i in range(10) ]
+points_count = 0
+frame = 0
 
+# def draw_big_point(p):      # 가야할 곳을 찍는다
+#     mouse_pointer.draw(p)
 
-def prepare_turtle_canvas():
-    turtle.setup(1024, 768)
-    turtle.bgcolor(0.2, 0.2, 0.2)
-    turtle.penup()
-    turtle.hideturtle()
-    turtle.shape('arrow')
-    turtle.shapesize(2)
-    turtle.pensize(5)
-    turtle.color(1, 0, 0)
-    turtle.speed(100)
-    turtle.goto(-500, 0)
-    turtle.pendown()
-    turtle.goto(480, 0)
-    turtle.stamp()
-    turtle.penup()
-    turtle.goto(0, -360)
-    turtle.pendown()
-    turtle.goto(0, 360)
-    turtle.setheading(90)
-    turtle.stamp()
-    turtle.penup()
-    turtle.home()
+# def draw_point(p):          # 점을 찍는다 (캐릭터가 이 좌표 하나하나를 따라가야함)
+#     character.clip_draw(frame * 66, 198, 66, 66, p[0], p[1], 150, 150)
 
-    turtle.shape('circle')
-    turtle.pensize(1)
-    turtle.color(0, 0, 0)
-    turtle.speed(50)
+def draw_line(p1, p2):       #  p1(x,y) 시작 p2(x,y) 도착
+    x1, y1 = p1[0], p1[1]    # 계산을 위해 좌표 넣어주고..
+    x2, y2 = p2[0], p2[1]    # ..
+    global frame
 
-    turtle.onkey(stop, 'Escape')
-    turtle.listen()
+    for i in range(0, 100 + 1, 3):
+        t = i / 100
+        x = (1-t) * x1 + t * x2  # 1-t : t 의 비율로 x1, x2를 섞는다 더한다.
+        y = (1-t) * y1 + t * y2
+        clear_canvas()
+        tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+        mx, my = points[points_count]
+        mouse_pointer.draw_now(mx, my)
+        if mx - x > 0:
+            character.clip_draw(frame * 66, 66, 66, 66, x, y, 150, 150)
+        elif mx - x < 0:
+            character.clip_draw(frame * 66, 132, 66, 66, x, y, 150, 150)
+        elif mx - x == 0:
+            if my - y > 0:
+                character.clip_draw(frame * 66, 0, 66, 66, x, y, 150, 150)
+            elif my - y < 0:
+                character.clip_draw(frame * 66, 198, 66, 66, x, y, 150, 150)
+        else:
+            character.clip_draw(frame * 66, 198, 66, 66, x, y, 150, 150)
+        frame = (frame + 1) % 4
+        update_canvas()
+        delay(0.03)
 
+# draw_point(p2)  # 마지막 점을 확실히 찍기
 
-def draw_big_point(p):
-    turtle.goto(p)
-    turtle.color(0.8, 0.9, 0)
-    turtle.dot(15)
-    turtle.write('     '+str(p))
+running = True
+mouse_pointer_drawing = True
+x = TUK_WIDTH // 2
+y = TUK_HEIGHT // 2
 
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 
-def draw_point(p):
-    turtle.goto(p)
-    turtle.dot(5, random.random(), random.random(), random.random())
+    draw_line((x,y), points[points_count])
 
+    x, y = points[points_count]
+    points_count += 1
+    delay(0.5)
 
-def draw_line(p1, p2):
-    draw_big_point(p1)
-    draw_big_point(p2)
+    if points_count == 9:
+        close_canvas()
 
-    x1, y1 = p1[0]. p1[1]
-    x2, y2 = p2[0]. p2[1]
-
-    a = (y2-y1)/(x2-x1)
-    b = y1 - x1 * a
-    for x in range (x1, x2+1, 10):
-        y = a * x + b
-        draw_point((x,y))
-
-    draw_point(p2)
-    pass
-
-
-prepare_turtle_canvas()
-
-draw_line((-100,-100),(300,150))
-
-turtle.done()
+close_canvas()
